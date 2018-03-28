ed73428f572902689ba46736ac4155402f41e26a
eric <yuanshuaiding@163.com>
2018-03-28 10:27:56 +0800

    优化嵌套函数

--- 函数/嵌套函数.py
+++ 函数/嵌套函数.py
@@ -2,10 +2,15 @@ age=19
 
 def fun1():
     age=20
-    print('fun1',"---->")
+    print('fun1',"---->",age)
     # 在函数内部定义的函数叫嵌套函数
     def fun2():
         print('fun2',"---->",age)
+        def fun3():
+            age=30
+            print('fun3', "---->", age)
+        fun3()
+            
 
     age=21
 
